4cb84465612c0a63b9d83c7f2904bb8abcd94775
wallison-bruno <wallyson-bruno@hotmail.com>
2020-04-26 22:20:51 -0300

    final

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   //?-> Essa 'camada' fica 'exposta' para que possa receber ou entrgar informações.
@@ -20,6 +21,7 @@ class _ContactPageState extends State<ContactPage> {
   Contact _editedContect;
   String nomeTitle = "Sem nome";
   ContactHelper helper = ContactHelper();
+  bool _userEdited = false;
 
   @override
   void initState() {
@@ -28,8 +30,10 @@ class _ContactPageState extends State<ContactPage> {
       //-> Atravez do 'widget' pode-se acessar 'ContactPage'
       _editedContect = Contact();
     } else {
-      //*_editedContect = Contact.fromMap(widget.contato.toMap()); -> Duplica o contato, pois era retornado um novo contato e não o própio contato.
-      _editedContect = widget.contato;
+      _editedContect = Contact.fromMap(widget.contato.toMap());
+      //-> Duplica o contato, pois será retornado um novo contato e não o própio contato em si(instância original).
+      //? duoplicando a instância do contato, ao retornar para paǵina de início, os valores instanciados não resram mostrados pois quem foi setado foi o contato duplicado.
+      // _editedContect = widget.contato; // Contato único para fazer o teste do estádo.
       _nameController.text = _editedContect.name;
       _emailController.text = _editedContect.email;
       _phoneController.text = _editedContect.phone;
@@ -39,119 +43,162 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text(_editedContect.name ?? nomeTitle),
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-        onPressed: () {
-          if (_editedContect.name != null && _editedContect.name.isNotEmpty) {
-            Navigator.pop(context, _editedContect);
-          } else {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
-        },
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              //? Quando coloca o TextFormField, estica a "caixa da coluna" e os itens se alinham.
-              child: Container(
-                width: 130,
-                height: 130,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: _editedContect.img != null
-                        ? FileImage(File(_editedContect.img))
-                        : AssetImage('images/person.png'),
+    return WillPopScope(
+      // -> 'WillPopScope' chama uma função quando a appBar apresenta 'pop' altomático.
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text(_editedContect.name ?? nomeTitle),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+          onPressed: () {
+            if (_editedContect.name != null && _editedContect.name.isNotEmpty) {
+              Navigator.pop(context, _editedContect);
+              //O segundo paremetro do pop() permite enviar para a pagina anterior um objeto.
+            } else {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
+          },
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              GestureDetector(
+                //? Quando coloca o TextFormField, estica a "caixa da coluna" e os itens se alinham.
+                child: Container(
+                  width: 130,
+                  height: 130,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _editedContect.img != null
+                          ? FileImage(File(_editedContect.img))
+                          : AssetImage('images/person.png'),
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  ImagePicker.pickImage(source: ImageSource.camera)
+                      .then((file) {
+                    if (file == null) return;
+                    setState(() {
+                      _editedContect.img = file.path;
+                    });
+                  });
+                },
+              ),
+              TextFormField(
+                /* O 'controller:' server capturar o que esta no 
+                  campo digitado pelo usuário e para colocar da-
+                  dos nos campos. */
+                controller: _nameController,
+                focusNode: _nameFocus,
+                onChanged: (text) {
+                  _userEdited = true;
+                  setState(() {
+                    nomeTitle = text;
+                  }); //-> para atualizar toda vez que digitado uma letra
+
+                  _editedContect.name = text;
+                },
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                ),
+                maxLines: 1,
+                keyboardType: TextInputType.text,
+                autofocus: false,
+                decoration: new InputDecoration(
+                  labelText: 'Nome Completo',
+                  hintText: 'Ex: Ana Lívia',
+                  labelStyle: TextStyle(
+                    color: Colors.red,
                   ),
                 ),
               ),
-            ),
-            TextFormField(
-              /* O 'controller:' server capturar o que esta no 
-                campo digitado pelo usuário e para colocar da-
-                dos nos campos. */
-              controller: _nameController,
-              focusNode: _nameFocus,
-              onChanged: (text) {
-                setState(() {
-                  print(text);
-                  nomeTitle = text;
-                  _editedContect.name = text;
-                });
-              },
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 18,
-              ),
-              maxLines: 1,
-              keyboardType: TextInputType.text,
-              autofocus: false,
-              decoration: new InputDecoration(
-                labelText: 'Nome Completo',
-                hintText: 'Ex: Ana Lívia',
-                labelStyle: TextStyle(
+              TextFormField(
+                controller: _emailController,
+                style: TextStyle(
                   color: Colors.red,
+                  fontSize: 18,
                 ),
-              ),
-            ),
-            TextFormField(
-              controller: _emailController,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 18,
-              ),
-              maxLines: 1,
-              keyboardType: TextInputType.emailAddress,
-              autofocus: false,
-              decoration: new InputDecoration(
-                labelText: 'Email',
-                hintText: 'nome@agenda.com',
-                labelStyle: TextStyle(
-                  color: Colors.red,
+                maxLines: 1,
+                keyboardType: TextInputType.emailAddress,
+                autofocus: false,
+                decoration: new InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'nome@agenda.com',
+                  labelStyle: TextStyle(
+                    color: Colors.red,
+                  ),
                 ),
-              ),
-              onChanged: (text) {
-                setState(() {
-                  print(text);
+                onChanged: (text) {
+                  _userEdited = true;
                   _editedContect.email = text;
-                });
-              },
-            ),
-            TextFormField(
-              controller: _phoneController,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 18,
+                },
               ),
-              maxLines: 1,
-              keyboardType: TextInputType.phone,
-              autofocus: false,
-              decoration: new InputDecoration(
-                labelText: 'Telefone',
-                hintText: '(00) 000-000-000',
-                labelStyle: TextStyle(
+              TextFormField(
+                controller: _phoneController,
+                style: TextStyle(
                   color: Colors.red,
+                  fontSize: 18,
                 ),
-              ),
-              onChanged: (text) {
-                setState(() {
-                  print(text);
+                maxLines: 1,
+                keyboardType: TextInputType.phone,
+                autofocus: false,
+                decoration: new InputDecoration(
+                  labelText: 'Telefone',
+                  hintText: '(00) 000-000-000',
+                  labelStyle: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+                onChanged: (text) {
+                  _userEdited = true;
                   _editedContect.phone = text;
-                });
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _requestPop() {
+    if (_userEdited) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+                title: Text('Descartar Alterações?'),
+                content: Text('Se sair as alterações seram perdias.'),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('Cancelar'),
+                    onPressed: () {
+                      Navigator.pop(context); // Saida manual
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('Sim'),
+                    onPressed: () {
+                      Navigator.pop(context); // Saida manual
+                      Navigator.pop(context); // Saida manual
+                      //Para poder voltar duas telas
+                    },
+                  ),
+                ]);
+          });
+
+      return Future.value(false); // Para não sair altomaticamente da tela
+    } else {
+      return Future.value(true); // Para sair altomaticamente da tela
+    }
   }
 }
--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -3,6 +3,9 @@ import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+enum OrderOptions { orderaz, orderza } //* -> Contantes
 
 class HomePage extends StatefulWidget {
   @override
@@ -37,6 +40,21 @@ class _HomePageState extends State<HomePage> {
         title: Text('Contatos'),
         backgroundColor: Colors.red,
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton<OrderOptions>(
+            itemBuilder: (context) => <PopupMenuEntry<OrderOptions>>[
+              const PopupMenuItem<OrderOptions>(
+                child: Text('Ordernar de A-Z'),
+                value: OrderOptions.orderaz,
+              ),
+              const PopupMenuItem<OrderOptions>(
+                child: Text('Ordernar de Z-A'),
+                value: OrderOptions.orderza,
+              ),
+            ],
+            onSelected: _orderList,
+          ),
+        ],
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
@@ -107,9 +125,75 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       onTap: () {
-        _showContactPage(contact: listContacts[index]);
+        _showOptions(context, index);
       },
     );
+  }
+
+  void _showOptions(BuildContext context, int index) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return BottomSheet(
+            onClosing: () {},
+            //Funsao que é executada quando o BottomSheet fecha
+            builder: (context) {
+              return Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  //-> Ocupar o mínimo de espaço porssível no eixo principal da coluna
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          'Ligar',
+                          style: TextStyle(color: Colors.red, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          launch('tel:${listContacts[index].phone}');
+                          //-> Uma url com comandos predeterminados para serem utilizados.
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          'Editar',
+                          style: TextStyle(color: Colors.red, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context); //-> sai do 'BottomSheet'
+                          _showContactPage(contact: listContacts[index]);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: FlatButton(
+                        child: Text(
+                          'Excluir',
+                          style: TextStyle(color: Colors.red, fontSize: 20),
+                        ),
+                        onPressed: () {
+                          helper.deliteContact(listContacts[index].id);
+                          //-> remove primeiro do banco d.
+                          setState(() {
+                            listContacts.removeAt(index);
+                            //-> depois remove da lista setada para mostrar os contatos.
+                          });
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        });
   }
 
   void _showContactPage({Contact contact}) async {
@@ -136,5 +220,21 @@ class _HomePageState extends State<HomePage> {
         _getAllContact();
       });
     }
+  }
+
+  void _orderList(OrderOptions result) {
+    switch (result) {
+      case OrderOptions.orderaz:
+        listContacts.sort((a, b) {
+          return a.name.toLowerCase().compareTo(b.name.toLowerCase());
+        });
+        break;
+      case OrderOptions.orderza:
+        listContacts.sort((a, b) {
+          return b.name.toLowerCase().compareTo(a.name.toLowerCase());
+        });
+        break;
+    }
+    setState(() {});
   }
 }
